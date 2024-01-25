b4a1e4e91c76c94c81e298b679c7a34a406c0343
arpit <arpitjain25.11.2001@gmail.com>
2024-01-25 18:06:13 +0530

    fifth commit

--- database.js
+++ database.js
@@ -1,15 +1,25 @@
 import  mysql from "mysql";
 
 
+// const db = mysql.createPool({
+//     connectionLimit: 100,
+//     host: "127.0.0.1",       
+//     user: "kumkum",       
+//     password: "password#123", 
+//     database: "userdb",     
+//     port: "3306"           
+//  })
+ 
+
 const db = mysql.createPool({
     connectionLimit: 100,
-    host: "127.0.0.1",       
-    user: "kumkum",       
-    password: "password#123", 
-    database: "userdb",     
-    port: "3306"           
- })
- 
+    host: process.env.DB_HOST || "127.0.0.1",
+    user: process.env.DB_USER || "kumkum",
+    password: process.env.DB_PASSWORD || "password#123",
+    database: process.env.DB_DATABASE || "userdb",
+    port: process.env.DB_PORT || "3306"
+});
+
  
 
  
